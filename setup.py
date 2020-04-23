b71d09a669ecb2bac10612dbeff8254748ac6454
Dirk Barbi <dirk.barbi@awi.de>
2020-04-23 10:28:16 +0200

    Bump version: 3.2.2 → 3.2.3

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dbarbi/esm_runscripts',
-    version='3.2.2',
+    version='3.2.3',
     zip_safe=False,
 )
