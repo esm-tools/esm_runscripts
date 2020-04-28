82ef76d0dc0ccc43563bb33a5b8f20e46ce610bb
Dirk Barbi <dirk.barbi@awi.de>
2020-04-28 11:43:39 +0200

    repaired version numbers

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dbarbi/esm_runscripts',
-    version="version='4.0.0'",
+    version="4.0.0",
     zip_safe=False,
 )
