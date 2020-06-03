ddb0c7710bb5bbe571183f6850a498bc96774217
Paul Gierz <pgierz@awi.de>
2020-06-03 13:16:34 +0200

    Bump version: 4.0.3 → 4.0.4

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dbarbi/esm_runscripts',
-    version="4.0.3",
+    version="4.0.4",
     zip_safe=False,
 )
