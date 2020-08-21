71322ed8a13891c7bbb183aa097e044f0b95e9e4
Paul Gierz <pgierz@awi.de>
2020-08-21 12:36:08 +0200

    some cleanup

--- esm_runscripts/namelists.py
+++ esm_runscripts/namelists.py
@@ -1,17 +1,52 @@
+import esm_parser
 import f90nml
+import logging
+import os
 import six
+import sys
+import warnings
 
 
-class namelist:
-
+class Namelist:
+    """Methods for dealing with FORTRAN namelists"""
     @staticmethod
     def nmls_load(mconfig):
-        import os
-        import logging
+        """
+        Loads Fortran namelists into the configuration dictionary. The
+        namelists are represented by f90nml Namelist objects, and are
+        stored under::
+
+            mconfig["namelists"]["namelist.echam"]``
+
+        This would point to the ECHAM namelist as a f90nml object, which
+        closely resembles a dictionary.
+
+        The actual namelists to load are listed in the raw configuration as a
+        list of strings::
+
+            mconfig['namelists'] = ['nml1', 'nml2', 'nml3', ...]
+
+        Namelists are assumed to have been copied to
+        ``mconfig["thisrun_config_dir"]``, and are loaded from there.
+
+        If the ``mconfig`` has a key ``"namelist_case"`` equal to "uppercase",
+        the uppercase attribute of the f90nml representation of the namelist is
+        set to ``True``.
+
+        Parameters
+        ----------
+        mconfig : dict
+            The model (e.g. ECHAM, FESOM, NEMO or OIFS) configuration
+
+        Returns
+        -------
+        mconfig : dict
+            The modified configuration.
+        """
         nmls = mconfig.get("namelists", [])
         mconfig["namelists"] = dict.fromkeys(nmls)
         for nml in nmls:
-            if os.path.isfile( os.path.join(mconfig["thisrun_config_dir"], nml)):
+            if os.path.isfile(os.path.join(mconfig["thisrun_config_dir"], nml)):
                 logging.debug("Loading %s", nml)
                 mconfig["namelists"][nml] = f90nml.read(
                     os.path.join(mconfig["thisrun_config_dir"], nml)
@@ -24,8 +59,6 @@ class namelist:
 
     @staticmethod
     def nmls_remove(mconfig):
-        import esm_parser
-        import logging
         namelist_changes = mconfig.get("namelist_changes", {})
         namelist_removes = []
         esm_parser.pprint_config(namelist_changes)
@@ -49,27 +82,21 @@ class namelist:
                 del mconfig["namelists"][namelist][change_chapter][key]
         return mconfig
 
-
     @staticmethod
     def nmls_modify(mconfig):
-        import six
         namelist_changes = mconfig.get("namelist_changes", {})
         for namelist, changes in six.iteritems(namelist_changes):
             print(str(mconfig["namelists"][namelist]))
             mconfig["namelists"][namelist].patch(changes)
         return mconfig
 
-
     @staticmethod
     def nmls_finalize(mconfig):
-        import os
-        import sys
-        import six
         all_nmls = {}
         for nml_name, nml_obj in six.iteritems(mconfig.get("namelists", {})):
             with open(os.path.join(mconfig["thisrun_config_dir"], nml_name), "w") as nml_file:
                 nml_obj.write(nml_file)
-            all_nmls[nml_name] = nml_obj  # PG or a string representation?
+            all_nmls[nml_name] = nml_obj  # PG: or a string representation?
         six.print_(
             "\n" "- Namelists modified according to experiment specifications..."
         )
@@ -79,3 +106,13 @@ class namelist:
             six.print_("\n", 40 * "+ ")
         return mconfig
 
+
+def namelist(Namelist):
+    """Legacy class name. Please use Namelist instead!"""
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            "Please change your code to use Namelist!",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        super(namelist, self).__init__(*args, **kwargs)
