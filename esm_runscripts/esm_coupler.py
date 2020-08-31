bb9b905c88372879af0203a23d274601c3b552ba
Sebastian Wahl <swahl@geomar.de>
2020-08-31 21:12:24 +0200

    switched to get to avoid dependency on latest version of esm_tools

--- esm_runscripts/esm_coupler.py
+++ esm_runscripts/esm_coupler.py
@@ -19,10 +19,10 @@ class esm_coupler:
             from . import oasis
             
             # seb-wahl: manual merge from 'oifs' branch as oifs branch contains many whitespace changes
-            self.norestart = full_config["oasis3mct"]["norestart"]
+            self.norestart = full_config["oasis3mct"].get("norestart","F")
             self.coupler = oasis.oasis(self.nb_of_couplings,  self.coupled_execs, self.runtime,
-                                       nnorest=self.norestart, mct_version=full_config["oasis3mct"]["mct_version"],
-                                       lucia=full_config["oasis3mct"]["use_lucia"])
+                                       nnorest=self.norestart, mct_version=full_config["oasis3mct"].get("mct_version",2.8),
+                                       lucia=full_config["oasis3mct"].get("use_lucia",False)
         else:
             print ("Unknown coupler :", name)
             sys.exit(0)
