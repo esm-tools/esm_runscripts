169a8ce4cb0a08e6e7866e7b47b143fcb4046d9d
Paul Gierz <pgierz@awi.de>
2020-09-03 15:10:17 +0200

    minor cleanup in filelists

--- esm_runscripts/filelists.py
+++ esm_runscripts/filelists.py
@@ -22,7 +22,7 @@ def rename_sources_to_targets(config):
                     if not config[model][filetype + "_sources"] == config[model][filetype + "_in_work"]:
                         print ("Mismatch between " + filetype + "_sources and " + filetype + "_in_work in model " + model)
                         sys.exit(-1)
-    
+
                 elif sources and targets and not in_work:
                     # all fine
                     pass
@@ -45,7 +45,7 @@ def rename_sources_to_targets(config):
                     if not config[model][filetype + "_targets"] == config[model][filetype + "_in_work"]:
                         print ("Mismatch between " + filetype + "_targets and " + filetype + "_in_work in model " + model)
                         sys.exit(-1)
-    
+
                 elif sources and targets and not in_work:
                     # all fine
                     pass
@@ -62,7 +62,7 @@ def rename_sources_to_targets(config):
                         config[model][filetype + "_targets"]= {}
                         for descrip, name in six.iteritems(config[model][filetype + "_sources"]):
                             config[model][filetype + "_targets"].update({ descrip: os.path.basename(name) })
-                        
+
             if in_work:
                 del config[model][filetype + "_in_work"]
 
@@ -149,13 +149,13 @@ def globbing(config):
                             newdescr = descr + "_glob_" + str(running_index)
                             config[model][filetype + "_sources"][newdescr] = new_filename
                             if config[model][filetype +  "_targets"][descr] == filename: #source and target are identical if autocompleted
-                                config[model][filetype + "_targets"][newdescr] = os.path.basename(new_filename) 
+                                config[model][filetype + "_targets"][newdescr] = os.path.basename(new_filename)
                             else:
                                 config[model][filetype + "_targets"][newdescr] = config[model][filetype + "_targets"][descr]
                             running_index += 1
 
                         del config[model][filetype + "_targets"][descr]
-    
+
     return config
 
 
@@ -174,7 +174,7 @@ def target_subfolders(config):
                             sys.exit(-1)
                         if "*" in filename:
                             source_filename = os.path.basename(config[model][filetype + "_sources"][descr])
-                            # directory wildcards are given as /*, wildcards in filenames are handled 
+                            # directory wildcards are given as /*, wildcards in filenames are handled
                             # seb-wahl: directory wildcards are given as /*, wildcards in filenames are handled
                             # in routine 'globbing' above, if we don't check here, wildcards are handled twice
                             # for files and hence filenames of e.g. restart files are screwed up.
@@ -191,8 +191,6 @@ def target_subfolders(config):
 
 def complete_restart_in(config):
     import esm_parser
-    print("BEFORE COMPLETE RESTART: ")
-    esm_parser.pprint_config(config)
     for model in config["general"]["valid_model_names"]:
         if not config[model]["lresume"] and config["general"]["run_number"] == 1:
             if "restart_in_sources" in config[model]:
@@ -203,11 +201,6 @@ def complete_restart_in(config):
             for categ in list(config[model]["restart_in_sources"].keys()):
                 if not config[model]["restart_in_sources"][categ].startswith("/"):
                     config[model]["restart_in_sources"][categ] = config[model]["parent_restart_dir"] + config[model]["restart_in_sources"][categ]
-
-
-    print("AFTER COMPLETE RESTART: ")
-    esm_parser.pprint_config(config)
-
     return config
 
 
@@ -233,7 +226,7 @@ def assemble_intermediate_files_and_finalize_targets(config):
                     else:
                         target_dir = (config["general"]["thisrun_work_dir"]).replace("//", "/")
 
-                        
+
                     config[model][filetype + "_intermediate"][category] =  interm_dir + target_name
                     config[model][filetype + "_targets"][category] =  target_dir + target_name
 
@@ -251,7 +244,7 @@ def replace_year_placeholder(config):
                         if file_category in config[model][filetype + "_targets"]:
                             if "@YEAR@" in config[model][filetype + "_targets"][file_category]:
                                 all_years = [config["general"]["current_date"].year]
-                                
+
                                 if (
                                    "need_timestep_before" in config[model][filetype + "_additional_information"][file_category]
                                 ):
@@ -277,10 +270,10 @@ def replace_year_placeholder(config):
                                     new_target_name = config[model][filetype + "_targets"][file_category].replace("@YEAR@", str(year))
                                     new_source_name = config[model][filetype + "_sources"][file_category].replace("@YEAR@", str(year))
 
-                                    config[model][filetype + "_targets"][new_category] = new_target_name 
+                                    config[model][filetype + "_targets"][new_category] = new_target_name
                                     config[model][filetype + "_sources"][new_category] = new_source_name
 
-                                del config[model][filetype + "_targets"][file_category] 
+                                del config[model][filetype + "_targets"][file_category]
                                 del config[model][filetype + "_sources"][file_category]
 
     return config
@@ -380,7 +373,7 @@ def check_for_unknown_files(config):
     index = 0
 
     for thisfile in all_files:
-        
+
 
         if os.path.realpath(thisfile) in known_files + unknown_files:
             continue
@@ -392,10 +385,10 @@ def check_for_unknown_files(config):
         config["general"]["unknown_sources"][index] = os.path.realpath(thisfile)
         config["general"]["unknown_targets"][index] = os.path.realpath(thisfile).replace(os.path.realpath(config["general"]["thisrun_work_dir"]), os.path.realpath(config["general"]["experiment_unknown_dir"]))
         config["general"]["unknown_intermediate"][index] = os.path.realpath(thisfile).replace(os.path.realpath(config["general"]["thisrun_work_dir"]), os.path.realpath(config["general"]["thisrun_unknown_dir"]))
-            
+
         unknown_files.append(os.path.realpath(thisfile))
 
-        index += 1        
+        index += 1
         print ("File is not in list: " + os.path.realpath(thisfile) )
 
     return config
