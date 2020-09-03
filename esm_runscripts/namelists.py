ea0f180f9ca19007c8c27f6e764ee554c43c29d2
Paul Gierz <pgierz@awi.de>
2020-09-03 15:09:40 +0200

    cleanup namelists for release 4.2 (with docs)

--- esm_runscripts/namelists.py
+++ esm_runscripts/namelists.py
@@ -1,19 +1,43 @@
-import esm_parser
-import f90nml
+"""
+``esm-runscripts`` Core Plugins for dealing with Fortran Namelists.
+
+Provides plugins for loading, modifying, deleting, and writing Fortran
+Namelists as part of the ``esm-runscripts`` recipe. All plugins are found under
+the class Namelist as static methods. A deprecated class ``namelist`` (small "n") is
+provided, which warns you when it is used.
+"""
 import logging
 import os
-import six
 import sys
 import warnings
+
+import f90nml
+import six
 
 
 class Namelist:
     """Methods for dealing with FORTRAN namelists"""
+
     @staticmethod
     def nmls_load(mconfig):
         """
-        Loads Fortran namelists into the configuration dictionary. The
-        namelists are represented by f90nml Namelist objects, and are
+        Loads Fortran namelists into the configuration dictionary.
+
+        User Information
+        ----------------
+        To associate namelists with a specific model, you should have a section
+        in your configuration that lists the namelists::
+
+            fesom:
+                namelists:
+                    - "namelist.config"
+                    - "namelist.oce"
+                    - "namelist.ice"
+                    - "namelist.diag"
+
+        Programmer Information
+        ----------------------
+        The namelists are represented by f90nml Namelist objects, and are
         stored under::
 
             mconfig["namelists"]["namelist.echam"]``
@@ -59,13 +83,41 @@ class Namelist:
 
     @staticmethod
     def nmls_remove(mconfig):
+        """
+        Removes an element from a namelist chapter.
+
+        User Information
+        ----------------
+        In the configuration file, assume you have::
+
+            echam:
+                namelist_changes:
+                    namelist.echam:
+                        radctl:
+                            co2vmr: "remove_from_namelist"
+
+        In this case, the entry co2vmr would be deleted from the radctl section
+        of namelist.echam.
+
+        Programmer Information
+        ----------------------
+        IDEA(PG): Maybe we can provide examples of how these functions are used
+        in the code?
+
+        Parameters
+        ----------
+        mconfig : dict
+            The model (e.g. ECHAM, FESOM, NEMO or OIFS) configuration
+
+        Returns
+        -------
+        mconfig : dict
+            The modified configuration.
+        """
         namelist_changes = mconfig.get("namelist_changes", {})
         namelist_removes = []
-        esm_parser.pprint_config(namelist_changes)
-        print(list(namelist_changes))
         for namelist in list(namelist_changes):
             changes = namelist_changes[namelist]
-            esm_parser.pprint_config(changes)
             logging.debug("Determining remove entires for %s", namelist)
             logging.debug("All changes: %s", changes)
             for change_chapter in list(changes):
@@ -84,19 +136,89 @@ class Namelist:
 
     @staticmethod
     def nmls_modify(mconfig):
+        """
+        Performs namelist changes.
+
+        User Information
+        ----------------
+        In the configuration file, you should have a section as::
+
+            echam:
+                namelist_changes:
+                    namelist.echam:
+                        radctl:
+                            co2vmr: 1200e-6
+
+
+        This would change the value of the echam namelist (namelist.echam),
+        subsection radctl, entry co2vmr to the value 1200e-6.
+
+        Programmer Information
+        ----------------------
+        IDEA(PG): Maybe we can provide examples of how these functions are used
+        in the code?
+
+        Note
+        ----
+        Actual changes are performed by the f90nml package patch fuction. See
+        here: https://tinyurl.com/y4ydz363
+
+        Parameters
+        ----------
+        mconfig : dict
+            The model (e.g. ECHAM, FESOM, NEMO or OIFS) configuration
+
+        Returns
+        -------
+        mconfig : dict
+            The modified configuration.
+        """
         namelist_changes = mconfig.get("namelist_changes", {})
         for namelist, changes in six.iteritems(namelist_changes):
-            print(str(mconfig["namelists"][namelist]))
             mconfig["namelists"][namelist].patch(changes)
         return mconfig
 
     @staticmethod
     def nmls_finalize(mconfig):
+        """
+        Writes namelists to disk after all modifications have finished.
+
+        User Information
+        ----------------
+        Part of the main log output will be a section specifing the actual
+        namelists that have been used for your simulation, including all
+        relevant additions, removals, or changes.
+
+        Programmer Information
+        ----------------------
+        A copy of the f90nml object representations of the namelists is stored
+        under the dictionary key "namelist_objs", as a dictionary of
+        ("namelist_name", f90nml_objfect) key/value pairs.
+
+        Warning
+        -------
+        Removing this step from your recipe might result in a broken run,
+        as the namelists will not be present in their desired form! Even if
+        your model runs, it might not contain all user-required changes.
+
+        Parameters
+        ----------
+        mconfig : dict
+            The model (e.g. ECHAM, FESOM, NEMO or OIFS) configuration
+
+        Returns
+        -------
+        mconfig : dict
+            The modified configuration.
+        """
         all_nmls = {}
         for nml_name, nml_obj in six.iteritems(mconfig.get("namelists", {})):
-            with open(os.path.join(mconfig["thisrun_config_dir"], nml_name), "w") as nml_file:
+            with open(
+                os.path.join(mconfig["thisrun_config_dir"], nml_name), "w"
+            ) as nml_file:
                 nml_obj.write(nml_file)
             all_nmls[nml_name] = nml_obj  # PG: or a string representation?
+        mconfig["namelist_objs"] = all_nmls
         six.print_(
             "\n" "- Namelists modified according to experiment specifications..."
         )
@@ -109,6 +231,7 @@ class Namelist:
 
 class namelist(Namelist):
     """Legacy class name. Please use Namelist instead!"""
+
     def __init__(self, *args, **kwargs):
         warnings.warn(
             "Please change your code to use Namelist!",
