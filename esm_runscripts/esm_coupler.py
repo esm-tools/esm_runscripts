163c1162c24523c900c1a2049d2c506919092b1d
Nadine Wieters <nadine.wieters@awi.de>
2020-09-08 11:34:38 +0200

    Added yac in esm_coupler

--- esm_runscripts/esm_coupler.py
+++ esm_runscripts/esm_coupler.py
@@ -1,5 +1,5 @@
 import sys
-known_couplers = ["oasis3mct"]
+known_couplers = ["oasis3mct", "yac"]
 
 class esm_coupler:
 
@@ -17,12 +17,14 @@ class esm_coupler:
                 self.nb_of_couplings += len(list(full_config[self.name]["coupling_target_fields"][restart_file]))
         if name == "oasis3mct":
             from . import oasis
-            
             # seb-wahl: manual merge from 'oifs' branch as oifs branch contains many whitespace changes
             self.norestart = full_config["oasis3mct"].get("norestart","F")
             self.coupler = oasis.oasis(self.nb_of_couplings,  self.coupled_execs, self.runtime,
                                        nnorest=self.norestart, mct_version=full_config["oasis3mct"].get("mct_version", "2.8"),
                                        lucia=full_config["oasis3mct"].get("use_lucia",False))
+        elif name == "yac":
+            from . import yac
+            self.coupler = yac.yac(self.nb_of_couplings, self.coupled_execs, full_config[self.name]["grids"], self.runtime)
         else:
             print ("Unknown coupler :", name)
             sys.exit(0)
@@ -163,76 +165,83 @@ class esm_coupler:
     def add_couplings(self, full_config):
         self.coupler.next_coupling = 1
         if "coupling_target_fields" in full_config[self.name]:
-            for restart_file in list(full_config[self.name]["coupling_target_fields"]):
-                for coupling in full_config[self.name]["coupling_target_fields"][restart_file]:
-                        coupling = coupling.replace("<--", "%").replace("--", "&")
-                        leftside, rest = coupling.split("%")
-                        leftside=leftside.strip()
-                        interpolation, rightside = rest.split("&")
-                        rightside=rightside.strip()
-                        interpolation=interpolation.strip()
-                        if ":" in leftside:
-                            lefts = leftside.split(":")
-                        else:
-                            lefts = [leftside]
-                            
-                        if ":" in rightside:
-                            rights = rightside.split(":")
-                        else:
-                            rights = [rightside]
-
-                        if not len(lefts) == len(rights):
-                            print ("Left and right side of coupling don't match: ", coupling)
-                            sys.exit(0)
-
-                         
-                        left_grid = lgrid_info = None
-                        right_grid = rgrid_info = None
-
-                        for left, right in zip(lefts, rights):
-                            found_left = found_right = False
-                            for model in list(full_config):
-                                if "coupling_fields" in full_config[model]:
-                                    if left in full_config[model]["coupling_fields"]:
-                                        found_left = True
-                                        leftmodel = model
-                                        if not left_grid:
-                                            left_grid = full_config[model]["coupling_fields"][left]["grid"]
-                                            lgrid_info = full_config[model]["grids"][left_grid]
-                                        else:
-                                            if not left_grid == full_config[model]["coupling_fields"][left]["grid"]: 
-                                                print ("All fields coupled together need to exist on same grid")
-                                                sys.exit(0)
-                                    if right in full_config[model]["coupling_fields"]:
-                                        found_right =True
-                                        rightmodel = model
-                                        if not right_grid:
-                                            right_grid = full_config[model]["coupling_fields"][right]["grid"]
-                                            rgrid_info = full_config[model]["grids"][right_grid]
-                                        else:
-                                            if not right_grid == full_config[model]["coupling_fields"][right]["grid"]:
-                                                print ("All fields coupled together need to exist on same grid")
-                                                sys.exit(0)
-                                    if found_right and found_left:
-                                        break
-                            if not found_left:
-                                print("Coupling var not found: ", left)
-                            if not found_right:
-                                print("Coupling var not found: ", right)
-                            if not found_left or not found_right:
+            print('coupler_name: ', self.coupler.name)
+            if self.coupler.name == "oasis3mct":
+                for restart_file in list(full_config[self.name]["coupling_target_fields"]):
+                    for coupling in full_config[self.name]["coupling_target_fields"][restart_file]:
+                            coupling = coupling.replace("<--", "%").replace("--", "&")
+                            leftside, rest = coupling.split("%")
+                            leftside=leftside.strip()
+                            interpolation, rightside = rest.split("&")
+                            rightside=rightside.strip()
+                            interpolation=interpolation.strip()
+                            if ":" in leftside:
+                                lefts = leftside.split(":")
+                            else:
+                                lefts = [leftside]
+                            if ":" in rightside:
+                                rights = rightside.split(":")
+                            else:
+                                rights = [rightside]
+    
+                            if not len(lefts) == len(rights):
+                                print ("Left and right side of coupling don't match: ", coupling)
                                 sys.exit(0)
+
+                            left_grid = lgrid_info = None
+                            right_grid = rgrid_info = None
+    
+                            for left, right in zip(lefts, rights):
+                                found_left = found_right = False
+                                for model in list(full_config):
+                                    if "coupling_fields" in full_config[model]:
+                                        if left in full_config[model]["coupling_fields"]:
+                                            found_left = True
+                                            leftmodel = model
+                                            if not left_grid:
+                                                left_grid = full_config[model]["coupling_fields"][left]["grid"]
+                                                lgrid_info = full_config[model]["grids"][left_grid]
+                                            else:
+                                                if not left_grid == full_config[model]["coupling_fields"][left]["grid"]: 
+                                                    print ("All fields coupled together need to exist on same grid")
+                                                    sys.exit(0)
+                                        if right in full_config[model]["coupling_fields"]:
+                                            found_right =True
+                                            rightmodel = model
+                                            if not right_grid:
+                                                right_grid = full_config[model]["coupling_fields"][right]["grid"]
+                                                rgrid_info = full_config[model]["grids"][right_grid]
+                                            else:
+                                                if not right_grid == full_config[model]["coupling_fields"][right]["grid"]:
+                                                    print ("All fields coupled together need to exist on same grid")
+                                                    sys.exit(0)
+                                        if found_right and found_left:
+                                            break
+                                if not found_left:
+                                    print("Coupling var not found: ", left)
+                                if not found_right:
+                                    print("Coupling var not found: ", right)
+                                if not found_left or not found_right:
+                                    sys.exit(0)
                         
-                        direction_info = None
-                        if "coupling_directions" in full_config[self.name]:
-                            if right_grid+"->"+left_grid in full_config[self.name]["coupling_directions"]:
-                                direction_info=full_config[self.name]["coupling_directions"][right_grid+"->"+left_grid] 
-                        transf_info = None
-                        if "coupling_methods" in full_config[self.name]:
-                            if interpolation in full_config[self.name]["coupling_methods"]:
-                                transf_info=full_config[self.name]["coupling_methods"][interpolation]
-
-                        self.coupler.add_coupling(lefts, lgrid_info, rights, rgrid_info, direction_info, transf_info, restart_file, full_config[self.name]["coupling_time_step"], full_config[self.name]["lresume"])
-
+                            direction_info = None
+                            if "coupling_directions" in full_config[self.name]:
+                                if right_grid+"->"+left_grid in full_config[self.name]["coupling_directions"]:
+                                    direction_info=full_config[self.name]["coupling_directions"][right_grid+"->"+left_grid] 
+                            transf_info = None
+                            if "coupling_methods" in full_config[self.name]:
+                                if interpolation in full_config[self.name]["coupling_methods"]:
+                                    transf_info=full_config[self.name]["coupling_methods"][interpolation]
+                            self.coupler.add_coupling(lefts, lgrid_info, rights, rgrid_info, direction_info, transf_info, restart_file, full_config[self.name]["coupling_time_step"], full_config[self.name]["lresume"])
+            elif self.coupler.name == "yac":
+                if "coupling_directions" in full_config[self.name]:
+                    transient_id = 1
+                    for direction in list(full_config[self.name]["coupling_directions"]):
+                        self.coupler.add_coupling(direction,full_config[self.name])
+                        for coupling_field in full_config[self.name]["coupling_target_fields"][direction]:
+                            self.coupler.add_coupling_field(coupling_field, transient_id, direction, full_config[self.name])
+                            transient_id += 1
+                        self.coupler.finalize_coupling()
                         
 
 
