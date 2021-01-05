0df2f6eb203387181a6d744180598e360db32561
denizural <denizural86@gmail.com>
2021-01-05 11:55:53 +0100

    Bump version: 5.0.7 → 5.0.8

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dbarbi/esm_runscripts',
-    version="5.0.7",
+    version="5.0.8",
     zip_safe=False,
 )
