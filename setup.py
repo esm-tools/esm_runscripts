4ae6a1b48ec996a9fc057e25c80cb2bc48b2b087
Dirk Barbi <dirk.barbi@awi.de>
2021-01-13 09:28:02 +0100

    Bump version: 5.0.10 → 5.0.11

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dbarbi/esm_runscripts',
-    version="5.0.10",
+    version="5.0.11",
     zip_safe=False,
 )
