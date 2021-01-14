28a4726568dee1b38a1c8f30c57e31cc0ffa6f92
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2021-01-14 17:35:59 +0100

    notify restart file overwritting

--- esm_runscripts/oasis.py
+++ esm_runscripts/oasis.py
@@ -357,6 +357,8 @@ class oasis:
                 print (filelist)
         # MA: -O flag added to overwrite oasis restart files in case oasis creats them
         # before (i.e. when using LOCTRANS)
+        if os.path.isfile(filelist):
+            print(f"{filelist} already exits, overwriting")
         print("cdo -O merge " + filelist + " " + restart_file )#+ enddate)
         os.system("cdo -O merge " + filelist + " " + restart_file )# + enddate)
         rmlist = glob.glob("notimestep*")
