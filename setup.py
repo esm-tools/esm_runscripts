6ffc3c38f152b571ce7902ed4d98c27ae2ec0af2
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2021-01-28 14:58:11 +0100

    Bump version: 5.0.13 → 5.0.14

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dbarbi/esm_runscripts',
-    version="5.0.13",
+    version="5.0.14",
     zip_safe=False,
 )
