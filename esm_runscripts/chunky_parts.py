6c92c4a0757659feb130db4f718c3e4d66305924
Dirk Barbi <dirk.barbi@awi.de>
2021-02-23 13:53:13 +0100

    further work

--- esm_runscripts/chunky_parts.py
+++ esm_runscripts/chunky_parts.py
@@ -1,7 +1,43 @@
-def read_chunk_date_file(config):
-    chunk_date_file = config["general"]["expid"] + "_chunks.date"
+import os, copy
+import yaml
+import esm_parser
 
-    with open(chunk_date_file, "r") as chunk_dates:
+
+
+def setup_correct_chunk(chunk_config, resubmit = False):
+    chunk_config = initialize_chunk_date_file(chunk_config) # make sure file exists and points to NEXT run
+    if resubmit:
+        chunk_config = update_chunk_date_file(chunk_config)
+
+    chunk_config = read_chunk_date_file(chunk_config)
+    chunk_config = set_model_queue(chunk_config)
+    chunk_config = is_first_run_in_chunk(chunk_config)
+    chunk_config = is_last_run_in_chunk(chunk_config)
+    chunk_config = find_next_model_to_run(chunk_config)
+    chunk_config = find_next_chunk_number(chunk_config)
+    config = remove_unnecessary_stuff(chunk_config)
+    config = merge_in_setup_config(config)
+
+    return config
+
+
+
+def merge_in_setup_config(config):
+    # at this point, config should only have a general section
+    model_config = yaml read...
+
+    model_config = esm_parser.deep_update(config, model_config)
+    return model_config
+
+
+def remove_unnecessary_stuff(config):
+    for model in config["general"]["model_queue"]:
+        del config[model]
+    return config
+
+
+def read_chunk_date_file(config):
+    with open(config["general"]["chunk_date_file"], "r") as chunk_dates:
         chunk_number, setup_name, run_number, start_date = chunk_dates.read().split()
 
     config["general"]["setup_name"] = setup_name
@@ -10,15 +46,18 @@ def read_chunk_date_file(config):
 
 
 
-def write_chunk_date_file(config):
-    chunk_date_file = config["general"]["expid"] + "_chunks.date"  
-    with open(chunk_date_file, "x") as chunk_dates:
-        chunk_dates.write(config["general"]["next_chunk_number"] + " " + config["general"]["next_setup_name"])
+def initialize_chunk_date_file(config):
+    chunk_date_file = config["general"]["expid"] + "_chunks.date"
+    config["general"]["chunk_date_file"] = chunk_date_file
+    if not os.path.isfile(chunk_date_file):
+        with open(chunk_date_file, "x") as chunk_dates:
+            chunk_dates.write("1 " + config["general"]["model1"]["setup_name"])
     return config
 
 
-
-def pick_next_chunk(config):
+def update_chunk_date_file(config):
+    with open(config["general"]["chunk_date_file"], "x") as chunk_dates:
+        chunk_dates.write(config["general"]["next_chunk_number"] + " " + config["general"]["next_setup_name"])
     return config
 
 
@@ -43,9 +82,9 @@ def set_model_queue(config):
 
 def is_first_run_in_chunk(config):
     if config["general"]["run_number"] % config["general"]["this_chunk_size"] == 1:
-        config["general"]["last_run_in_chunk"] = True
+        config["general"]["first_run_in_chunk"] = True
     else:
-        config["general"]["last_run_in_chunk"] = False
+        config["general"]["first_run_in_chunk"] = False
     return config
 
 
