a4080c5c480a7d886a9468f144c37a36608c4660
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2021-08-26 07:15:09 +0200

    Merge pull request #180 from esm-tools/hist_fixed
    Hist fixed
    
    (cherry picked from commit 4036651ef40ebbaee69f71164926d9a3a56ce0af)

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/esm-tools/esm_runscripts',
-    version="5.1.28",
+    version="5.1.29",
     zip_safe=False,
 )
