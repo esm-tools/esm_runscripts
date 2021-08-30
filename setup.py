667bc2373281e25cb7a33da92e459cc232bc8a0f
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2021-08-30 08:48:01 +0200

    Bump version: 5.1.29 → 5.1.30

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/esm-tools/esm_runscripts',
-    version="5.1.29",
+    version="5.1.30",
     zip_safe=False,
 )
