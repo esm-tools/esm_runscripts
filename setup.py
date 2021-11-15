cbf3203cd88d837cc418142f21214b7d6ab77733
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2021-11-15 12:43:43 +0100

    Merge pull request #202 from esm-tools/hotfix/namelist_remove_var_properties
    hotfix/namelist remove var properties
    
    (cherry picked from commit 51ca1d958ca40cc2f55024046c0c5d7211827c32)

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/esm-tools/esm_runscripts',
-    version="5.1.35",
+    version="5.1.36",
     zip_safe=False,
 )
